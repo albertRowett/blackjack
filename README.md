5098b0bdcfb9ec62e1a74395c9ce551c3e8858d6
Albert Rowett <139590109+albertRowett@users.noreply.github.com>
2023-11-10 11:34:52 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# website-template
+This template can be used as the basis of new websites.
+
+You can download the zip file, unzip/decompress it, copy the whole folder and rename it.
---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Template</title>
+
+    <meta name="description" content="Template HTML file">
+    <meta name="author" content="iO Academy">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+
+    <link rel="icon" href="images/favicon.png" sizes="192x192">
+    <link rel="shortcut icon" href="images/favicon.png">
+    <link rel="apple-touch-icon" href="images/favicon.png">
+
+    <script defer src="js/index.js"></script>
+</head>
+
+<body>
+
+<h1>Website Template</h1>
+
+</body>
+</html>
