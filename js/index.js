0c0b28b0a6aca6a4fbf8771a99be640ee408adf9
Albert Rowett <139590109+albertRowett@users.noreply.github.com>
2023-11-13 13:33:19 +0100

    Create array of cards (deck)

---
+++ js/index.js
@@ -0,0 +1,7 @@
+let deck = [
+    {twoC: 2}, {threeC: 3}, {fourC: 4}, {fiveC: 5}, {sixC: 6}, {sevenC: 7}, {eightC: 8}, {nineC: 9}, {tenC: 10}, {jackC: 10}, {queenC: 10}, {kingC: 10}, {aceC: 11},
+    {twoD: 2}, {threeD: 3}, {fourD: 4}, {fiveD: 5}, {sixD: 6}, {sevenD: 7}, {eightD: 8}, {nineD: 9}, {tenD: 10}, {jackD: 10}, {queenD: 10}, {kingD: 10}, {aceD: 11},
+    {twoH: 2}, {threeH: 3}, {fourH: 4}, {fiveH: 5}, {sixH: 6}, {sevenH: 7}, {eightH: 8}, {nineH: 9}, {tenH: 10}, {jackH: 10}, {queenH: 10}, {kingH: 10}, {aceH: 11},
+    {twoS: 2}, {threeS: 3}, {fourS: 4}, {fiveS: 5}, {sixS: 6}, {sevenS: 7}, {eightS: 8}, {nineS: 9}, {tenS: 10}, {jackS: 10}, {queenS: 10}, {kingS: 10}, {aceS: 11}
+]
+
