d15b12e5a27448829f6cbaf7ec1ea020dad71d47
Albert Rowett <139590109+albertRowett@users.noreply.github.com>
2023-11-13 14:37:38 +0100

    Create shuffle function. Install ESLint

--- js/index.js
+++ js/index.js
@@ -3,5 +3,14 @@ let deck = [
     {twoD: 2}, {threeD: 3}, {fourD: 4}, {fiveD: 5}, {sixD: 6}, {sevenD: 7}, {eightD: 8}, {nineD: 9}, {tenD: 10}, {jackD: 10}, {queenD: 10}, {kingD: 10}, {aceD: 11},
     {twoH: 2}, {threeH: 3}, {fourH: 4}, {fiveH: 5}, {sixH: 6}, {sevenH: 7}, {eightH: 8}, {nineH: 9}, {tenH: 10}, {jackH: 10}, {queenH: 10}, {kingH: 10}, {aceH: 11},
     {twoS: 2}, {threeS: 3}, {fourS: 4}, {fiveS: 5}, {sixS: 6}, {sevenS: 7}, {eightS: 8}, {nineS: 9}, {tenS: 10}, {jackS: 10}, {queenS: 10}, {kingS: 10}, {aceS: 11}
-]
+];
 
+// Fisher-Yates shuffle
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        console.log(i);
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
