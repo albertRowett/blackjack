009944ad37190ca60f52efe859e0276729e71337
Albert Rowett <139590109+albertRowett@users.noreply.github.com>
2023-12-07 19:29:28 +0000

    Refactor to enable resolution of multiple hands
    Modify existing code to enable subsequent hands to be resolved once ability to split is  implemented

--- js/index.js
+++ js/index.js
@@ -57,8 +57,87 @@ let deck = [
 let currentCard = -1;
 
 // Initial (empty) hands
-let player = { hands: [{ hand: [], cards: [], handValue: 0 }], wallet: 1000, bet: 0 };
-let dealer = { hands: [{ hand: [], cards: [], handValue: 0 }] };
+let player = { hands: [{ hand: [], cards: [], handValue: 0 }], currentHandIndex: 0, wallet: 1000, bet: 0 };
+let dealer = { hands: [{ hand: [], cards: [], handValue: 0 }], currentHandIndex: 0 };
+
+// Gameplay
+function playFirstHand() {
+    shuffle(deck);
+    deal(player);
+    deal(dealer);
+    deal(player);
+    deal(dealer);
+    updateConsole();
+
+    if (player.hands[0].handValue === 21) {
+        resolveBlackjack();
+    } else {
+        showButtons();
+    }
+}
+
+function playNextHand() {
+    player.currentHandIndex++;
+    deal(player);
+    updateConsole();
+    showButtons();
+}
+
+function shuffle(deck) {
+    for (let i = deck.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [deck[i], deck[j]] = [deck[j], deck[i]];
+    }
+
+    return deck;
+}
+
+function deal(person) {
+    currentCard++;
+    const dealtCard = deck[currentCard];
+    person.hands[person.currentHandIndex].hand.push(dealtCard);
+    person.hands[person.currentHandIndex].cards = updateCards(person);
+    person.hands[person.currentHandIndex].handValue = updateHandValue(person);
+}
+
+function updateCards(person) {
+    const cards = person.hands[person.currentHandIndex].hand.map((card) => {
+        return card.card;
+    });
+    return cards;
+}
+
+function updateHandValue(person) {
+    const cardValues = person.hands[person.currentHandIndex].hand.map((card) => {
+        return card.value;
+    });
+    const handValue = cardValues.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue;
+    }, 0);
+
+    return resolveAces(handValue, person.hands[person.currentHandIndex].cards);
+}
+
+function resolveAces(handValue, cards) {
+    if (handValue > 21) {
+        const aceIndex = cards.findIndex((card) => card.includes('ace'));
+        if (aceIndex !== -1) {
+            handValue -= 10;
+            let cardsWithoutAce = cards.toSpliced(aceIndex, 1);
+            return resolveAces(handValue, cardsWithoutAce);
+        }
+    }
+
+    return handValue;
+}
+
+function updateConsole() {
+    console.log('------------------');
+    console.log("Player's cards (hand " + convertHandIndexToHandNumber(player.currentHandIndex) + '): ' + player.hands[player.currentHandIndex].cards);
+    console.log("Player's score (hand " + convertHandIndexToHandNumber(player.currentHandIndex) + '): ' + player.hands[player.currentHandIndex].handValue);
+    console.log("Dealer's cards: " + dealer.hands[0].cards);
+    console.log("Dealer's score: " + dealer.hands[0].handValue);
+}
 
 // Event listeners
 document.querySelector('.betForm').addEventListener('submit', handleBetSubmit);
@@ -75,7 +154,7 @@ function handleBetSubmit(e) {
         if (bet <= player.wallet) {
             handleBet(bet, player);
             toggleBetFormVisibility();
-            startRound();
+            playFirstHand();
         } else {
             console.log('Bet must not be greater than wallet amount');
         }
@@ -96,174 +175,120 @@ function handleHitClick() {
     deal(player);
     updateConsole();
 
-    if (player.hands[0].handValue > 21) {
-        toggleHitStandButtonVisibility();
-        resolvePlayerBust();
-    } else if (player.hands[0].handValue === 21) {
-        toggleHitStandButtonVisibility();
-        resolveRound();
+    if (player.hands[player.currentHandIndex].handValue >= 21) {
+        hideHitStandButtons();
+        determineIfAllHandsPlayed();
     }
 }
 
 function handleStandClick() {
     hideSplitButton();
-    toggleHitStandButtonVisibility();
-    resolveRound();
+    hideHitStandButtons();
+    determineIfAllHandsPlayed();
 }
 
 function handleSplitClick() {
-    handleBet(player.bet, player);
-    const numberOfHands = splitHand(player);
-    console.log(numberOfHands);
+    // handleBet(player.bet, player);
+    // const numberOfHands = splitHand(player);
+    // console.log(numberOfHands);
 }
 
-function splitHand(player) {
-    const currentHand = player.hands[0];
-    player.hands.push({ hand: [currentHand.hand.pop()], cards: [], handValue: 0 });
+// function splitHand(player) {
+//     const currentHand = player.hands[0];
+//     player.hands.push({ hand: [currentHand.hand.pop()], cards: [], handValue: 0 });
 
-    return player.hands.length;
+//     return player.hands.length;
+// }
+
+function determineIfAllHandsPlayed() {
+    if (player.currentHandIndex + 1 < player.hands.length) {
+        playNextHand();
+    } else {
+        resolveDealerHand();
+    }
 }
 
-// Button visibility toggling
+function resolveDealerHand() {
+    if (dealer.hands[0].handValue < 17) {
+        deal(dealer);
+        updateConsole();
+        resolveDealerHand();
+    } else {
+        resolveBets();
+    }
+}
+
+// HTML element visibility toggling
 function toggleBetFormVisibility() {
     document.querySelector('.betForm').classList.toggle('hidden');
 }
 
-function toggleHitStandButtonVisibility() {
-    document.querySelector('.hitButton').classList.toggle('hidden');
-    document.querySelector('.standButton').classList.toggle('hidden');
+function showButtons() {
+    document.querySelector('.hitButton').classList.remove('hidden');
+    document.querySelector('.standButton').classList.remove('hidden');
+
+    if (player.hands[player.currentHandIndex].hand[0].value === player.hands[player.currentHandIndex].hand[1].value && player.wallet >= player.bet) {
+        document.querySelector('.splitButton').classList.remove('hidden');
+    }
 }
 
-function showSplitButton() {
-    document.querySelector('.splitButton').classList.remove('hidden');
+function hideHitStandButtons() {
+    document.querySelector('.hitButton').classList.add('hidden');
+    document.querySelector('.standButton').classList.add('hidden');
 }
 
 function hideSplitButton() {
     document.querySelector('.splitButton').classList.add('hidden');
 }
 
-// Gameplay
-function startRound() {
-    shuffle(deck);
-    deal(player);
-    deal(dealer);
-    deal(player);
-    deal(dealer);
-    updateConsole();
-
-    if (player.hands[0].handValue === 21) {
-        resolveBlackjack();
-    } else {
-        toggleHitStandButtonVisibility();
-
-        if (player.hands[0].hand[0].value === player.hands[0].hand[1].value && player.wallet >= player.bet) {
-            showSplitButton();
-        }
-    }
-}
-
-function shuffle(deck) {
-    for (let i = deck.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [deck[i], deck[j]] = [deck[j], deck[i]];
-    }
-
-    return deck;
-}
-
-function deal(person) {
-    currentCard++;
-    const dealtCard = deck[currentCard];
-    person.hands[0].hand.push(dealtCard);
-    person.hands[0].cards = updateCards(person);
-    person.hands[0].handValue = updateHandValue(person);
-}
-
-function updateCards(person) {
-    const cards = person.hands[0].hand.map((card) => {
-        return card.card;
-    });
-    return cards;
-}
-
-function updateHandValue(person) {
-    const cardValues = person.hands[0].hand.map((card) => {
-        return card.value;
-    });
-    const handValue = cardValues.reduce((accumulator, currentValue) => {
-        return accumulator + currentValue;
-    }, 0);
-
-    return resolveAces(handValue, person.hands[0].cards);
-}
-
-function resolveAces(handValue, cards) {
-    if (handValue > 21) {
-        const aceIndex = cards.findIndex((card) => card.includes('ace'));
-        if (aceIndex !== -1) {
-            handValue -= 10;
-            let cardsWithoutAce = cards.toSpliced(aceIndex, 1);
-            return resolveAces(handValue, cardsWithoutAce);
-        }
-    }
-
-    return handValue;
-}
-
-function updateConsole() {
-    console.log('------------------');
-    console.log("Player's cards: " + player.hands[0].cards);
-    console.log("Player's score: " + player.hands[0].handValue);
-    console.log("Dealer's cards: " + dealer.hands[0].cards);
-    console.log("Dealer's score: " + dealer.hands[0].handValue);
-}
-
 // Round resolution
 function resolveBlackjack() {
     if (dealer.hands[0].handValue === 21) {
-        console.log('Round outcome: blackjack- draw');
+        console.log('Blackjack- draw');
         player.wallet += parseInt(player.bet);
     } else {
-        console.log('Round outcome: blackjack- player wins');
+        console.log('Blackjack- player wins');
         player.wallet += 2.5 * parseInt(player.bet);
     }
 
     prepareNewRound(player, dealer);
 }
 
-function resolvePlayerBust() {
-    console.log('Round outcome: bust- dealer wins');
+function resolveBets() {
+    for (let i = 0; i < player.hands.length; i++) {
+        if (player.hands[i].handValue > 21) {
+            console.log('Hand ' + convertHandIndexToHandNumber(i) + ': bust- dealer wins');
+        } else {
+            if (dealer.hands[0].handValue > 21) {
+                console.log('Hand ' + convertHandIndexToHandNumber(i) + ': dealer bust- player wins');
+                player.wallet += 2 * parseInt(player.bet);
+            } else {
+                if (dealer.hands[0].handValue > player.hands[i].handValue) {
+                    console.log('Hand ' + convertHandIndexToHandNumber(i) + ': dealer wins');
+                } else if (dealer.hands[0].handValue < player.hands[i].handValue) {
+                    console.log('Hand ' + convertHandIndexToHandNumber(i) + ': player wins');
+                    player.wallet += 2 * parseInt(player.bet);
+                } else {
+                    console.log('Hand ' + convertHandIndexToHandNumber(i) + ': draw');
+                    player.wallet += parseInt(player.bet);
+                }
+            }
+        }
+    }
+
     prepareNewRound(player, dealer);
 }
 
-function resolveRound() {
-    if (dealer.hands[0].handValue < 17) {
-        deal(dealer);
-        updateConsole();
-        resolveRound();
-    } else {
-        if (dealer.hands[0].handValue > 21) {
-            console.log('Round outcome: dealer bust- player wins');
-            player.wallet += 2 * parseInt(player.bet);
-        } else {
-            if (dealer.hands[0].handValue > player.hands[0].handValue) {
-                console.log('Round outcome: dealer wins');
-            } else if (dealer.hands[0].handValue < player.hands[0].handValue) {
-                console.log('Round outcome: player wins');
-                player.wallet += 2 * parseInt(player.bet);
-            } else {
-                console.log('Round outcome: draw');
-                player.wallet += parseInt(player.bet);
-            }
-        }
+function convertHandIndexToHandNumber(handIndex) {
+    const handNumber = handIndex + 1;
 
-        prepareNewRound(player, dealer);
-    }
+    return handNumber;
 }
 
 function prepareNewRound(player, dealer) {
     if (player.wallet > 0) {
         player.hands = [{ hand: [], cards: [], handValue: 0 }];
+        player.currentHandIndex = 0;
         dealer.hands = [{ hand: [], cards: [], handValue: 0 }];
         currentCard = -1;
         toggleBetFormVisibility();
