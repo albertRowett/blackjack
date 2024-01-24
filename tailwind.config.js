cf54ce29b7344c7e4745fe011ac005b857b1cdae
Albert Rowett <139590109+albertRowett@users.noreply.github.com>
2024-01-24 10:44:05 +0100

    Style middle section
    Style buttons, card areas and score indicators

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-        extend: {},
+        extend: {
+            dropShadow: {
+                'rb': '1.5px 1.5px 0 rgba(0, 0, 0, 0.5)'
+            }
+        },
     },
     plugins: []
 }
